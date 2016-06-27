e250dc8e44179814ea185aca9de4cbf0d1ed3946
Griddummy <griddummy@gmail.com>
2016-06-27 17:53:32 +0900

    player 프로퍼티 수정 완료
    player 프로퍼티 수정 완료

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour {
     private int m_HpRegenerateBuff;   // 분당 회복력 버프
 
     private int m_Sp;                 // spellPower, 현재 SP
-    private int m_SpMaxIncrease;      // Buff sp (장비로 인한 추가 sp)
+    private int m_SpMaxBuff;      // Buff sp (장비로 인한 추가 sp)
     private int m_SpRegenerateBuff;   // 분당 회복력 버프
 
     private int m_AttackPowerBuff;    // 공격력 버프
@@ -32,25 +32,22 @@ public class Player : MonoBehaviour {
         {
             if (value < 0)
                 value = 0;
-            else if (value > HpMax)
-                value = HpMax;
+            else if (value > hpMax)
+                value = hpMax;
 
             m_Hp = value;
         }
     }
 
-    public int HpMax // Max HP를 얻는다 (기본 MaxHP + Buff MaxHP ) getp   
+    public int hpMax // Max HP를 얻는다 (기본 MaxHP + Buff MaxHP ) get   
     {
         get
         {
-            return m_Hp + m_HpMaxBuff;
-        }
-        set
-        {
-            HpMax = value;
+            return getLevelInfo().maxHP + hpMaxBuff;
         }
     }
-    public int HpMaxBuff // 아이템으로 인한 Max Hp 증가량 제어 get, set
+
+    public int hpMaxBuff // 아이템으로 인한 Max Hp 증가량 제어 get, set
     {
         get
         {
@@ -61,21 +58,133 @@ public class Player : MonoBehaviour {
             m_HpMaxBuff = value;
         }
     }
-    public int HpRegenerate; // HP 분당 회복량을 얻는다. (기본 + 버프) get        
-    public int HpRegenerateBuff; // 아이템으로 인한 HP 분당 회복량을 제어 get, set
 
-    public int Sp; // 현재 Sp를 제어 get,set
-    public int SpMax; // 현재 Max SP를 얻는다.( 기본 Max SP + Buff Max SP) get
-    public int SpMaxBuff; // 아이템으로 인한 Max Sp 증가량 제어 get, set
-    public int SpRegenerate; // SP 분당 회복량을 얻는다. get
-    public int SpRegenerateBuff; // 아이템으로 인한 SP 분당 회복량 제어 get, set
+    public int hpRegenerate // HP 분당 회복량을 얻는다. (기본 + 버프) get        
+    {
+        get
+        {
+            return getLevelInfo().hpRegenerate + m_HpRegenerateBuff;
+        }
+    }
+    
+    public int hpRegenerateBuff  // 아이템으로 인한 HP 분당 회복량을 제어 get, set
+    {
+        get
+        {
+            return m_HpRegenerateBuff;
+        }
+        set
+        {
+            m_HpRegenerateBuff = value;
+        }
+    }
 
-    public int AttackPower;      // 전체 공격력을 얻는다 (기본 + 버프) get
-    public int BaseAttackPower;  // 기본 공격력을 얻는다. get
-    public int AttackPowerBuff;  // 공격력 버프량(아이템으로 인한)을 제어 get, set
+    public int Sp // 현재 Sp를 제어 get,set
+    {
+        get
+        {
+            return m_Sp;
+        }
+        set
+        {
+            if (value < 0)
+                value = 0;
+            else if (value > spMax)
+                value = spMax;
 
-    public int Lv; //현재 레벨을 제어. get, set
-    public int Exp; // 현재 경험치 제어. get, set
+            m_Sp = value;
+        }
+    }
+
+    public int spMax // 현재 Max SP를 얻는다.( 기본 Max SP + Buff Max SP) get
+    {
+        get
+        {
+            return getLevelInfo().maxSP + spMaxBuff;
+        }
+    }
+
+    public int spMaxBuff // 아이템으로 인한 Max Sp 증가량 제어 get, set
+    {
+        get
+        {
+            return m_SpMaxBuff;
+        }
+        set
+        {
+            m_SpMaxBuff = value;
+        }
+    }
+
+    public int spRegenerate // SP 분당 회복량을 얻는다. get
+    {
+        get
+        {
+            return getLevelInfo().spRegenerate + m_SpRegenerateBuff;
+        }
+    }
+
+    public int spRegenerateBuff // 아이템으로 인한 SP 분당 회복량 제어 get, set
+    {
+        get
+        {
+            return m_SpRegenerateBuff;
+        }
+        set
+        {
+            m_SpRegenerateBuff = value;
+        }
+    }
+
+    public int attackPower      // 전체 공격력을 얻는다 (기본 + 버프) get
+    {
+        get
+        {
+            return getLevelInfo().attackPower + m_AttackPowerBuff;
+        }
+    }
+    public int baseAttackPower  // 기본 공격력을 얻는다. get
+    {
+        get
+        {
+            return getLevelInfo().attackPower;
+        }
+    }
+    public int attackPowerBuff  // 공격력 버프량(아이템으로 인한)을 제어 get, set
+    {
+        get
+        {
+            return m_AttackPowerBuff;
+        }
+
+        set
+        {
+            m_AttackPowerBuff = value;
+        }
+    }
+
+    public int lv //현재 레벨을 제어. get, set
+    {
+        get
+        {
+            return m_Level;
+        }
+        set
+        {
+            m_Level = value;
+        }
+    }
+    public int exp // 현재 경험치 제어. get, set
+    {
+        get
+        {
+            return m_Experience;
+        }
+        set
+        {
+            m_Experience = value;
+        }
+    }
 
     void Start()
     {
